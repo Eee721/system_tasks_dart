82bf04c77b1c0a5836a7b8b795729156d5bd46bd
Eee721 <454180070@qq.com>
2022-12-16 18:37:01 +0800

    nullable

--- example/system_tasks_dart_example.dart
+++ example/system_tasks_dart_example.dart
@@ -3,6 +3,12 @@ import 'package:system_tasks_dart/system_tasks_dart.dart';
 void main() async{
   var b = await SystemTasks.isRunning("E:\\working\\projects\\muflauncher\\.build\\launcher\\launcher.exe");
   print(b);
+
+  var task = await SystemTasks.getRunningTaskByPath("E:\\working\\projects\\muflauncher\\.build\\launcher\\launcher.exe");
+  if (task != null){
+    print(task);
+  }
+
   // SystemTasks.tasks(byName: "launcher.exe").then((ps) {
   //   // var p = ps.firstWhere((p) => p.pname.toLowerCase() == 'code.exe');
   //   // if (p != null) {
